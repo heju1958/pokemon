1e40b80cef2d1aa9e989be7c82aec6dd026211ff
helena <heju1958@outlook.com>
2023-03-26 10:42:34 -0300

    style: dashboard

--- pokescreen/src/pages/Dashboard/index.tsx
+++ pokescreen/src/pages/Dashboard/index.tsx
@@ -3,14 +3,18 @@ import Header from "../../components/Header";
 import Buttons from "../../components/Buttons";
 import SideBar from "../../components/SideBar";
 
+import { ContainerDashboard, BoxDashboard } from "./style";
+
 const Dashboard = () => {
   return (
-    <>
+    <ContainerDashboard>
       <SideBar />
-      {/* <Header />
-      <PokeCard />
-      <Buttons /> */}
-    </>
+      <BoxDashboard>
+        <Header />
+        <PokeCard />
+        <Buttons />
+      </BoxDashboard>
+    </ContainerDashboard>
   );
 };
 
---
+++ pokescreen/src/pages/Dashboard/style.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const ContainerDashboard = styled.div`
+  display: flex;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const BoxDashboard = styled.div`
+  margin: 2rem;
+  background-color: var(--color-primary);
+  border-radius: 2rem;
+  width: 100%;
+
+  @media (max-width: 768px) {
+    margin: 1rem;
+    width: 90%;
+  }
+`;
